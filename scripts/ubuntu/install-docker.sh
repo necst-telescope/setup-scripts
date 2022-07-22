3f5b3cb3eba11d66acc3d0bc821d53d2261f199a
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-07-22 13:54:49 +0900

    #3 Fix null file object path

--- scripts/ubuntu/install-docker.sh
+++ scripts/ubuntu/install-docker.sh
@@ -37,7 +37,7 @@ echo \
     "deb [arch=$(dpkg --print-architecture) \
     signed-by=/etc/apt/keyrings/docker.gpg] \
     https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
-    | sudo tee /etc/apt/sources.list.d/docker.list > dev/null
+    | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo chmod +r /etc/apt/keyrings/docker.gpg
 
 # Install Docker Engine
